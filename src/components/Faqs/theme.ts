12ea49608f360379125ade2637517aad345e410f
Nathaniel <yaroslav.v@biswap.dev>
2024-06-26 09:42:43 +0300

    some components, format, lint

---
+++ src/components/Faqs/theme.ts
@@ -0,0 +1,58 @@
+import { variants } from "./types";
+
+export const titleVariants = {
+  [variants.LIGHT]: {
+    color: "dark800",
+  },
+  [variants.DARK]: {
+    color: "white",
+  },
+  [variants.DARK_BACKGROUND]: {
+    color: "white",
+  },
+};
+export const wrapperVariants = {
+  [variants.LIGHT]: {
+    backgroundColor: "gray300",
+  },
+  [variants.DARK]: {
+    backgroundColor: "dark600",
+  },
+  [variants.DARK_BACKGROUND]: {
+    backgroundColor: "dark600",
+  },
+};
+export const questionVariants = {
+  [variants.LIGHT]: {
+    color: "dark800",
+  },
+  [variants.DARK]: {
+    color: "white",
+  },
+  [variants.DARK_BACKGROUND]: {
+    color: "white",
+  },
+};
+export const descriptionVariants = {
+  [variants.LIGHT]: {
+    color: "gray900",
+  },
+  [variants.DARK]: {
+    color: "gray700",
+  },
+  [variants.DARK_BACKGROUND]: {
+    color: "gray700",
+  },
+};
+
+export const openBackground = {
+  [variants.LIGHT]: {
+    backgroundColor: "transparent",
+  },
+  [variants.DARK]: {
+    backgroundColor: "transparent",
+  },
+  [variants.DARK_BACKGROUND]: {
+    backgroundColor: "dark900",
+  },
+};
