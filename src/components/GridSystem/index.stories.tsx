263e151af6eae459259688ae0c6d36f26ce929ac
Den-b6 <dmytro.z@biswap.dev>
2024-06-28 16:48:11 +0300

    refactor GridSystem component

--- src/components/GridSystem/index.stories.tsx
+++ src/components/GridSystem/index.stories.tsx
@@ -1,28 +1,29 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 
+// components
+import { Box, Grid } from "../Box";
+
 export default {
   title: "Components/GridSystem",
-  // component: GridSystem,
   argTypes: {},
 };
 
-const Wrapper = styled.div`
-  display: grid;
+const Wrapper = styled(Grid)`
   grid-template-columns: repeat(12, 1fr);
   grid-column-gap: 8px;
-
-  //max-width: 1200px;
   margin: 0 auto 24px;
 `;
-const Column = styled.div`
-  background: indianred;
+
+const Column = styled(Box)`
   padding: 12px;
+  background: ${({ theme }) => theme.colors.pastelBlue};
 `;
-const ColumnCustom = styled.div`
+
+const ColumnCustom = styled(Box)`
   grid-column: span 4;
-  background: cadetblue;
   padding: 12px;
+  background: ${({ theme }) => theme.colors.dark100};
 
   ${({ theme }) => theme.mediaQueries.xxl} {
     grid-column: span 3;
@@ -53,25 +54,6 @@ export const Default: FC = (): JSX.Element => {
         <ColumnCustom>3</ColumnCustom>
         <ColumnCustom>4</ColumnCustom>
       </Wrapper>
-
-      {/*<Wrapper2>*/}
-      {/*  <Column2>1</Column2>*/}
-      {/*  <Column2>2</Column2>*/}
-      {/*  <Column2>3</Column2>*/}
-      {/*  <Column2>4</Column2>*/}
-      {/*  <Column2>5</Column2>*/}
-      {/*  <Column2>6</Column2>*/}
-      {/*  <Column2>7</Column2>*/}
-      {/*  <Column2>8</Column2>*/}
-      {/*  <Column2>9</Column2>*/}
-      {/*  <Column2>10</Column2>*/}
-      {/*  <Column2>11</Column2>*/}
-      {/*  <Column2>12</Column2>*/}
-      {/*  <Column2>13</Column2>*/}
-      {/*  <Column2>14</Column2>*/}
-      {/*  <Column2>15</Column2>*/}
-      {/*  <Column2>16</Column2>*/}
-      {/*</Wrapper2>*/}
     </>
   );
 };
