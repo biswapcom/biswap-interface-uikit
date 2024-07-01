e0d7db0cb73f800113f9a964ac87826e22103ba6
Den-b6 <dmytro.z@biswap.dev>
2024-07-01 22:34:17 +0300

    refactor Layouts component

--- src/components/Layouts/CardsLayout.tsx
+++ src/components/Layouts/CardsLayout.tsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
+
+// components
 import BaseLayout from "./BaseLayout";
 
 const GridLayout = styled(BaseLayout)`
   & > div {
     grid-column: span 6;
+
     ${({ theme }) => theme.mediaQueries.sm} {
       grid-column: span 4;
     }
--- src/components/Layouts/ContainerCarousel.tsx
+++ src/components/Layouts/ContainerCarousel.tsx
@@ -1,9 +1,13 @@
 import React, { FC, ReactElement } from "react";
+
+// components
 import { Box, BoxProps } from "../Box";
+
+// utils
 import { getResponsiveAttrs } from "../../util";
 
 const ContainerCarousel: FC<BoxProps> = ({ children, ...props }): ReactElement => (
-  <Box px={getResponsiveAttrs({ xs: 0, sm: 0, md: "16px", lg: "24px", xxl: 0 })} mx="auto" maxWidth="1120px" {...props}>
+  <Box maxWidth="1120px" mx="auto" px={getResponsiveAttrs({ xs: 0, sm: 0, md: "16px", lg: "24px", xxl: 0 })} {...props}>
     {children}
   </Box>
 );
