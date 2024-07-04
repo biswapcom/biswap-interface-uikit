25dfddfdf5f95bc3774ec8e4ae1e514f7e677c22
Den-b6 <dmytro.z@biswap.dev>
2024-07-04 13:52:49 +0300

    build fix

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -2,5 +2,4 @@ export { default as Box } from "./Box";
 export { default as Flex } from "./Flex";
 export { default as Grid } from "./Grid";
 
-// @ts-ignore
-export { BoxProps, FlexProps, GridProps } from "./types";
+export type { BoxProps, FlexProps, GridProps } from "./types";
