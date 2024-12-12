70fdbf4f0eda206dea60ac61be917b6e81bc52ff
Nathaniel <yaroslav.v@biswap.dev>
2024-12-12 16:41:41 +0200

    fixes

--- dist/components/Svg/types.d.ts
+++ dist/components/Svg/types.d.ts
@@ -3,14 +3,14 @@ import { DefaultTheme } from "styled-components";
 import { SpaceProps } from "styled-system";
 import { Colors } from "../../theme";
 export interface SvgProps extends SVGAttributes<HTMLOrSVGElement>, SpaceProps {
-    theme?: DefaultTheme;
-    spin?: boolean;
+  theme?: DefaultTheme;
+  spin?: boolean;
 }
 export type IconComponentType = {
-    iconName: string;
-    isActive?: boolean;
-    height?: string;
-    width?: string;
-    activeColor?: string;
-    activeBackgroundColor?: keyof Colors;
+  iconName: string;
+  isActive?: boolean;
+  height?: string;
+  width?: string;
+  activeColor?: string;
+  activeBackgroundColor?: keyof Colors;
 } & SvgProps;
