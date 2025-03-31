f1ac98c9af4bdbe26eb515d01a8352f177a0f6c1
Den-b6 <dmytro.z@biswap.dev>
2025-03-31 12:18:07 +0300

    Add OptionRIcon to Svg components
    Introduced a new OptionRIcon component and updated the `Svg` component index to export it. This addition also includes the corresponding type definition for the new icon.

--- dist/components/Svg/index.d.ts
+++ dist/components/Svg/index.d.ts
@@ -270,5 +270,6 @@ export { default as AttentionRoundRIcon } from "./Icons/New/AttentionRoundR";
 export { default as InfoRoundRIcon } from "./Icons/New/InfoRoundR";
 export { default as HelpRoundRIcon } from "./Icons/New/HelpRoundR";
 export { default as AddLiquidityRIcon } from "./Icons/New/AddLiquidityR";
+export { default as OptionRIcon } from "./Icons/New/OptionR";
 export { default as Svg } from "./Svg";
 export type { SvgProps } from "./types";
