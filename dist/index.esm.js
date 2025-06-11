dff0e783df5500d29c8595091e97af990a60212e
Den-b6 <dmytro.z@biswap.dev>
2025-06-11 10:30:16 +0300

    Enhance SVG stroke color with theming support
    Replaced hardcoded stroke color with a dynamic theming solution using `getColor`. Removed unnecessary comments and unused code for improved readability and maintainability.

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1989,38 +1989,17 @@ const defaultObject = {
 };
 const getResponsiveAttrs = (obj) => Object.values({ ...defaultObject, ...obj });
 
+const getColor$2 = ({ color, theme }) => {
+    return getThemeValue(`colors.${color}`, color)(theme);
+};
 const Icon$3 = (props) => {
-    useTheme();
+    const theme = useTheme();
     return (React.createElement(Svg, { viewBox: "0 0 80 80", fill: "currentColor", ...props },
-        React.createElement("rect", { x: "2", y: "2", width: "76", height: "76", rx: "38", 
-            // stroke={getColor({ color: props.color, theme })}
-            // fill="none"
-            stroke: "currentColor", fill: "none", strokeWidth: "4" }),
+        React.createElement("rect", { x: "2", y: "2", width: "76", height: "76", rx: "38", stroke: getColor$2({ color: props.color, theme }), fill: "none", strokeWidth: "4" }),
         React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M21.666 40C21.666 29.9129 29.9122 21.6667 39.9993 21.6667C50.0865 21.6667 58.3327 29.9129 58.3327 40C58.3327 50.0871 50.0865 58.3333 39.9993 58.3333C29.9122 58.3333 21.666 50.0871 21.666 40ZM39.9993 25C31.7532 25 24.9993 31.7538 24.9993 40C24.9993 48.2462 31.7532 55 39.9993 55C48.2455 55 54.9993 48.2462 54.9993 40C54.9993 31.7538 48.2455 25 39.9993 25Z" }),
         React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M40.0007 31.6667C40.9211 31.6667 41.6673 32.4129 41.6673 33.3333V41.6667C41.6673 42.5871 40.9211 43.3333 40.0007 43.3333C39.0802 43.3333 38.334 42.5871 38.334 41.6667V33.3333C38.334 32.4129 39.0802 31.6667 40.0007 31.6667Z" }),
         React.createElement("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M38.1582 46.6667C38.1582 45.6541 38.979 44.8333 39.9915 44.8333H40.0065C41.019 44.8333 41.8398 45.6541 41.8398 46.6667C41.8398 47.6792 41.019 48.5 40.0065 48.5H39.9915C38.979 48.5 38.1582 47.6792 38.1582 46.6667Z" })));
 };
-// <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-//   <rect x="2" y="2" width="76" height="76" rx="38" stroke="#FFDB1C" strokeWidth="4" />
-//   <path
-//     fillRule="evenodd"
-//     clipRule="evenodd"
-//     d="M21.666 40C21.666 29.9129 29.9122 21.6667 39.9993 21.6667C50.0865 21.6667 58.3327 29.9129 58.3327 40C58.3327 50.0871 50.0865 58.3333 39.9993 58.3333C29.9122 58.3333 21.666 50.0871 21.666 40ZM39.9993 25C31.7532 25 24.9993 31.7538 24.9993 40C24.9993 48.2462 31.7532 55 39.9993 55C48.2455 55 54.9993 48.2462 54.9993 40C54.9993 31.7538 48.2455 25 39.9993 25Z"
-//     fill="#FFDB1C"
-//   />
-//   <path
-//     fillRule="evenodd"
-//     clipRule="evenodd"
-//     d="M40.0007 31.6667C40.9211 31.6667 41.6673 32.4129 41.6673 33.3333V41.6667C41.6673 42.5871 40.9211 43.3333 40.0007 43.3333C39.0802 43.3333 38.334 42.5871 38.334 41.6667V33.3333C38.334 32.4129 39.0802 31.6667 40.0007 31.6667Z"
-//     fill="#FFDB1C"
-//   />
-//   <path
-//     fillRule="evenodd"
-//     clipRule="evenodd"
-//     d="M38.1582 46.6667C38.1582 45.6541 38.979 44.8333 39.9915 44.8333H40.0065C41.019 44.8333 41.8398 45.6541 41.8398 46.6667C41.8398 47.6792 41.019 48.5 40.0065 48.5H39.9915C38.979 48.5 38.1582 47.6792 38.1582 46.6667Z"
-//     fill="#FFDB1C"
-//   />
-// </svg>;
 
 const Icon$2 = (props) => {
     return (React.createElement(Svg, { viewBox: "0 0 24 24", fill: "currentColor", ...props },
