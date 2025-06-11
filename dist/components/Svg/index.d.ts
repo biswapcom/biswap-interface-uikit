64ecb1c6921f12f73f99d75b42ca3cb2ec7acd20
Den-b6 <dmytro.z@biswap.dev>
2025-06-11 10:18:47 +0300

    Add AttentionCircleRIcon component
    Introduced a new `AttentionCircleRIcon` SVG component and included its export in the `Svg` index. The implementation includes theming support and adheres to styling conventions.

--- dist/components/Svg/index.d.ts
+++ dist/components/Svg/index.d.ts
@@ -269,6 +269,7 @@ export { default as SwapVerticalRIcon } from "./Icons/New/SwapVerticalR";
 export { default as SwapHorizontalRIcon } from "./Icons/New/SwapHorizontalR";
 export { default as CheckRoundRIcon } from "./Icons/New/CheckRoundR";
 export { default as AttentionRoundRIcon } from "./Icons/New/AttentionRoundR";
+export { default as AttentionCircleRIcon } from "./Icons/New/AttentionCircleR";
 export { default as InfoRoundRIcon } from "./Icons/New/InfoRoundR";
 export { default as HelpRoundRIcon } from "./Icons/New/HelpRoundR";
 export { default as AddLiquidityRIcon } from "./Icons/New/AddLiquidityR";
